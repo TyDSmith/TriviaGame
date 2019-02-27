964d1b9a5ed3376c1053771cb0f99026e58f2fff
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-02-27 13:04:30 -0800

    js logic additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,20 @@ var timerMinutes = 4;
 var timerSeconds = 59;
 var timeLeft = null;
 timerId = null;
-var correctAnswers = 0;
+var correctAnsCount = 0;
 var incorrectAnswers = 0;
+
+let qNumber = 0;
+let question = "";
+let answer1 = "";
+let answer2 = "";
+let answer3 = "";
+let answer4 = "";
 
 
 var myQuestions = [
     {
+        qHead: "Question #1",
         question: "What is the top speed of a Mountain Lion?",
         answers: {
             a: '50 MPH',
@@ -19,6 +27,7 @@ var myQuestions = [
         correctAnswer: 'a'
     },
     {
+        qHead: "Question #2",
         question: "What is the average lifespan of a Mountain Lion in the wild?",
         answers: {
             a: '15 years',
@@ -29,6 +38,7 @@ var myQuestions = [
         correctAnswer: 'b'
     },
     {
+        qHead: "Question #3",
         question: "How many pounds of meat does an adult Mountain Lion require each day?",
         answers: {
             a: '5 lbs.',
@@ -39,6 +49,7 @@ var myQuestions = [
         correctAnswer: 'c'
     },
     {
+        qHead: "Question #4",
         question: "Are Mountain Lions Dangerous?",
         answers: {
             a: 'No',
@@ -52,14 +63,34 @@ var myQuestions = [
 
 
 
-
-
 function setup(){
 
     timerId = setInterval("handleTimer();", 1000);
     $("#timer-minutes").html(timerMinutes);
     $("#timer-seconds").html(timerSeconds);
 };
+
+//dislay questions
+function showQuestions(){
+
+};
+
+//create eventlistener to watch clicks
+
+
+//see if clicked answer is correct
+function checkAnswer(e){
+    let clickedDiv = e.currentTarget;
+    if (clickedDiv.innerText === myQuestions[qNumber].correctAnswer){
+        correctAnsCount++;
+    }
+};
+
+function showResults(){
+
+};
+
+
 
 function handleTimer(){
     timerSeconds--;
@@ -76,13 +107,9 @@ function handleTimer(){
         $("#timer-minutes").html(timerMinutes);
     };
 
-    if(timerMinutes == 0){
+    if(timerMinutes == 0 && timerSeconds == 0){
         console.log("Time's up!");
         alert("Time is up!");
     }
-
-};
-
-function showResults(){
 
 };
